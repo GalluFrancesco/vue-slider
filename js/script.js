8e405be2e641d5f961964297b67a2112f6a68546
GalluFrancesco <latuamaildigithub@email.comfrancesco.gallu@gmail.com>
2022-01-16 10:42:01 +0100

    Add: array img

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
 new Vue({
     el:"#app",
     data:{
+        images: [
+            'https://unsplash.it/300/300?image=110',
+            'https://unsplash.it/300/300?image=120',
+            'https://unsplash.it/300/300?image=130',
+            'https://unsplash.it/300/300?image=140',
+            'https://unsplash.it/300/300?image=150',
+            'https://unsplash.it/300/300?image=160',
+        ],
         
     },
     methods:{
