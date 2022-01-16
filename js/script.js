895538163135f0526b34a1bd637dc6430c779a59
GalluFrancesco <latuamaildigithub@email.comfrancesco.gallu@gmail.com>
2022-01-16 11:55:49 +0100

    Add: html e logica pulsanti next|prev

--- js/script.js
+++ js/script.js
@@ -1,17 +1,34 @@
 new Vue({
     el:"#app",
     data:{
+        currentIndex: 0,
         images: [
             'https://unsplash.it/300/300?image=110',
             'https://unsplash.it/300/300?image=120',
             'https://unsplash.it/300/300?image=130',
             'https://unsplash.it/300/300?image=140',
-            'https://unsplash.it/300/300?image=150',
+            'https://unsplash.it/300/300?image=151',
             'https://unsplash.it/300/300?image=160',
-        ],
+        ]
         
     },
     methods:{
+        nextIMG: function () {
+            this.currentIndex++;
+            //se siamo arrivati all'ultima immagine dello slider
+            //setta il currentIndex a 0
+            if(this.currentIndex === this.images.length)
+                {this.currentIndex=0}
+        },
+        prevIMG: function () {
+            this.currentIndex--;
+            //se siamo arrivati alla prima immagine dello slider
+            //setta il currentIndex all'ultima posizione
+            //della lista di immagini
+            if(this.currentIndex < 0)
+                {this.currentIndex=this.images.length-1}
+            
+        }
 
     }
 })
